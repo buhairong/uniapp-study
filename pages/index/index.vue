d94b7693b0a767264511105d02861ed6a84c0b2b
hairong.pu.o <hairong.pu.o@nio.com>
2021-11-22 13:18:55 +0800

    静态轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,48 @@
 <template>
-	<view class="content">
-		<u-button>按钮</u-button>
+	<view id="index">
+		<!-- 页面 header 相关部分 -->
+		<view class="header-box">
+			<!-- 顶部广告位轮播图 -->
+			<swiper class="swiper" :indicator-dots="false" :autoplay="true" :interval="2500" :duration="500">
+				<swiper-item>
+					<navigator open-type="navigate" :url=" '/pages/webview/webview?url='+encodeURI('http://baidu.com')">
+						<image class="banner-swiper-img" src="http://ns-strategy.cdn.bcdbos.com/ns-strategy/upload/fc_big_pic/part-00140-2668.jpg" mode="aspectFill" />
+					</navigator>
+				</swiper-item>
+				<swiper-item>
+					<navigator open-type="navigate" :url=" '/pages/webview/webview?url='+encodeURI('http://baidu.com')">
+						<image class="banner-swiper-img" src="http://ns-strategy.cdn.bcdbos.com/ns-strategy/upload/fc_big_pic/part-00140-2668.jpg" mode="aspectFill" />
+					</navigator>
+				</swiper-item>
+				<swiper-item>
+					<navigator open-type="navigate" :url=" '/pages/webview/webview?url='+encodeURI('http://baidu.com')">
+						<image class="banner-swiper-img" src="http://ns-strategy.cdn.bcdbos.com/ns-strategy/upload/fc_big_pic/part-00140-2668.jpg" mode="aspectFill" />
+					</navigator>
+				</swiper-item>
+			</swiper>
+			<!-- 遮罩使用弧形框 -->
+			<image class="crile" src="@/static/crile.png" mode="aspectFill" />
+			<!-- 两个选项按钮 -->
+			<view class="card-header">
+				<view class="card-one card-left" @tap="gotoFeeds('/pages/feeds/feeds')">
+					<image class="img" src="@/static/coffee.png" mode="aspectFill" />
+					<view class="iright">
+						<view class="title">精彩动态</view>
+					</view>
+				</view>
+				<view class="card-one card-right" @tap="gotoFeeds('/pages/me/me')">
+					<image class="img" src="@/static/ran.png" mode="aspectFill" />
+					<view class="iright">
+						<view class="title">个人中心</view>
+					</view>
+				</view>
+			</view>
+			<!-- Tab 选项卡 -->
+			<view class="tabs-box">
+		 		<view class="one-nav">推荐</view>
+		 		<view class="one-nav">资讯</view>
+		 	</view>
+		</view>
 	</view>
 </template>
 
@@ -15,35 +57,132 @@
 
 		},
 		methods: {
-
+			gotoFeeds(url) {
+				uni.switchTab({
+					url
+				})
+			}
 		}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss" scoped>
+	.header-box {
+		position: relative;
+		left: 0;
+		height: 500upx;
+		background-color: #f1f1f1;
+		z-index: 1;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+		// 广告位轮播器相关样式
+		.swiper {
+			width: 750upx;
+			height: 400upx;
+			position: absolute;
+			left: 0;
+			top: 0;
+			text-align: center;
+			z-index: 1;
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+			.banner-swiper-img {
+				width: 750upx;
+				height: 400upx;
+				box-shadow: 0 0 2px 0 rgb(188, 188, 188);
+			}
+		}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+		.crile {
+			width: 750upx;
+			height: 50upx;
+			position: absolute;
+			left: 0;
+			top: 355upx;
+			z-index: 9;
+		}
+
+		// 新鲜事 活动墙 按钮样式
+		.card-header {
+			position: absolute;
+			left: 0;
+			top: 320upx;
+			height: 160upx;
+			z-index: 99;
+			width: 710upx;
+			margin-left: 20upx;
+			display: flex;
+			flex-direction: row;
+			flex-wrap: wrap;
+			justify-content: space-between;
+			align-items: center;
+			align-content: center;
+
+			.card-one {
+				width: 328upx;
+				height: 96upx;
+				border-radius: 49upx;
+				background-color: #fff;
+				margin: 0 10upx;
+				box-shadow: 0 0 2px 0 rgb(188, 188, 188);
+				display: flex;
+				flex-direction: row;
+				flex-wrap: wrap;
+				justify-content: flex-start;
+				align-items: center;
+				align-content: center;
+
+				.img {
+					width: 44upx;
+					height: 44upx;
+					margin-left: 50upx;
+				}
+
+				.iright {
+					margin-left: 30upx;
+					text-align: left;
+					color: #888;
+
+					.title {
+						font-size: 30upx;
+						color: #001432;
+					}
+
+					.iview {
+						display: flex;
+						flex-direction: row;
+						flex-wrap: wrap;
+						justify-content: space-between;
+						align-items: center;
+						align-content: center;
+						font-size: 20upx;
+						margin-top: -5upx;
+					}
+				}
+			}
+		}
+
+		// 推荐、咨询 按钮样式
+		.tabs-box {
+			width: 750upx;
+			position: absolute;
+			z-index: 1;
+			left: 0;
+			top: 480upx;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+
+			.one-nav {
+				height: 80upx;
+				width: 110upx;
+				color: #9B9B9B;
+				font-size: 36upx;
+				text-align: center;
+				font-weight: blod;
+
+				&.nav-actived {
+					color: #0050FF;
+				}
+			}
+		}
 	}
 </style>
